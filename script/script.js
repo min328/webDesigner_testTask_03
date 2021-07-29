4b14bfd27179b0d0a4769c2076f77513cd326777
min328 <kmj20212021@gmail.com>
2021-07-29 13:27:36 +0900

    newly developed

--- script/script.js
+++ script/script.js
@@ -1,11 +1,22 @@
 $(function(){
 
-    $("li.mainLi").mouseover(function(){
-        $(this).children("ul.subMenu").stop().slideDown(600);
+    // gnb Menu
+    $("#headerGnb li.mainLi").mouseover(function(){
+        $(this).children("ul.subMenu").stop().fadeIn(300);
+    });
+    $("#headerGnb li.mainLi").mouseout(function(){
+        $(this).children("ul.subMenu").stop().fadeOut(500);
     });
 
-    $("li.mainLi").mouseout(function(){
-        $(this).children("ul.subMenu").stop().fadeOut(200);
-    });
+    // setInterval(slideShow, 1000);
+    function slideShow() {
+        $("div.slideImage").eq(0).fadeOut(
+            1000,
+            function(){
+                $(this).insertAfter("div.slideImage:last-child");
+            }
+        );
+        $("div.slideImage").eq(1).fadeIn(1000);
+    }
 
-});
+    }); // 기본 template
